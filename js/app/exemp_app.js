2adb4f439aa0ae8ab11abaff2d73731d27a03626
Ilton Garcia <ilton_unb@hotmail.com>
2014-02-05 09:59:55 -0200

    Refatoração para que a tela de Login seja feita a de Mobile quando for um acesso Mobile e uma normal quando vinda de um Desktop em função do tamanho da tela. Reforação também da acoplagem do framework à solução exemplo.

--- js/app/exemp_app.js
+++ js/app/exemp_app.js
@@ -1,11 +1,30 @@
-ModuloTonFaceJS.controller('AcoesFBCtrl', function ($scope, $http) {
+app_id = 229992460513152;
 
+ModuloTonFaceJS.controller('AcoesFBCtrl', function ($scope, $http) {
+    
 	$scope.SetUp = function(){
-		setUpFB();
+            // Configuração da função a ser executada após o SetUp
+            func_executada_onSetUp = function(response) {
+                if (response.status === 'connected') {
+                    console.log('Logged in on FB');
+                    $('#acoes_fb').hide();
+                    $('#resultado_fb').show();
+                }else FB.login();
+
+                usuario_corrente();
+            }
+            
+            setUpFB(app_id);
 	}
 
-	$scope.AssinarApp = function(){
-		assinar_app();
+	$scope.AssinarApp = function() {
+            url_redirect = 'tonjs.herokuapp.com/';
+            
+            assinar_app(app_id, url_redirect);
+	}
+
+	$scope.LogOut = function(){
+		deslogar_app();
 	}
 	
 });
--- js/tonface.js
+++ js/tonface.js
@@ -1,28 +1,25 @@
-function assinar_app(){
-	url_base_fb_login = 'http://www.facebook.com/dialog/oauth?client_id=';
-	app_id = 229992460513152;
-	param_redirect = '&redirect_uri=http://';
-	val_redirect = 'tonjs.herokuapp.com/';
-	param_touch = '/&display=touch&state=';
+/**
+ * Esta variável deve ser preenchida com a função a ser executada depois que o setUp do FB seja concluído
+ * Obs: if (response.status === 'connected') significa que a permissão foi dada pelo usuário corrente
+ * @type function com @param response
+ */
+var func_executada_onSetUp;
 
-	document.location = url_base_fb_login + app_id + param_redirect + val_redirect + param_touch;
-}
-
-function setUpFB(){
+/**
+ * Função que faz a inicialização do FB JS SDK
+ * Obs: Lembre de configurar a variável func_executada_onLogin com a função a ser executada no SetUp
+ * @param {int} app_id => ID identificador do App em developers.facebook.com
+ * @returns {null}
+ */
+function setUpFB(app_id) {
 	window.fbAsyncInit = function() {
 	    FB.init({
-	        appId      : '229992460513152',
+	        appId      : app_id,
 	        status     : true,
 	        xfbml      : true
 	    });
 
-		FB.Event.subscribe('auth.authResponseChange', function(response) {
-			if (response.status === 'connected') {
-				console.log('Logged in on FB');
-				$('#acoes_fb').hide();
-				$('#resultado_fb').show();
-			}else FB.login();
-		});
+            FB.Event.subscribe('auth.authResponseChange', func_executada_onSetUp);
 	};
 
 	(function(d){
@@ -31,4 +28,45 @@ function setUpFB(){
 	    js.src = "//connect.facebook.net/es_LA/all.js";
 	    d.getElementsByTagName('head')[0].appendChild(js);
 	}(document));
+}
+
+/**
+ * Função que redireciona o usuário à tela específica (mobile ou desktop) de Login do FB
+ * @param {int} app_id => ID do App que está lá no developers.facebook.com
+ * @param {string} url_redirect => URL da aplicação, é aonde o facebook vai redirecionar depois da operação
+ * @returns {null}
+ */
+function assinar_app(app_id, url_redirect){
+	url_base_fb_login = 'http://www.facebook.com/dialog/oauth?client_id=';
+        param_redirect = '&redirect_uri=http://';
+        param_touch = '';
+
+        // Os parametros foram o tamanho da tela do iPad
+        if( screen.height <= 770 && screen.width <= 1024 ) eh_mobile = true;
+        else eh_mobile = false;
+
+        //CRIA O LINK PARA LOGIN MOBILE, SEM ELE O LOGIN É DESKTOP
+        if(eh_mobile == true) param_touch = '/&display=touch&state=';
+
+	document.location = url_base_fb_login + app_id + param_redirect + url_redirect + param_touch;
+}
+
+/**
+ * Desloga o usuário do App e também do facebook
+ * @returns {null}
+ */
+function deslogar_app(){
+	FB.logout(function(response) {
+		console.log(response);
+	});
+}
+
+/**
+ * Pega a sessao de usuário atual (/me)
+ * @returns {null}
+ */
+function usuario_corrente(){
+	FB.api('/me', function(response) {
+            console.log(response);
+	});	
 }
